aa4b167e881f96fa20d19ce4585863a5f8e6379e
Yash Matharu <ymatharu7@gmail.com>
2021-08-12 12:16:07 -0400

    Connect header to correct pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import { Fragment } from "react";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Header from "./components/Layout/Header";
 
 // TEMPORARY
 import Container from "react-bootstrap/Container";
-// import Products from "./components/Products/Products";
+import Products from "./components/Products/Products";
 // import Footer from "./components/Layout/Footer/Footer";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [showCart, setShowCart] = useState(false);
+
+  const showCartHandler = () => {
+    setShowCart(true);
+  };
+
+  const hideCartHandler = () => {
+    setShowCart(false);
+  };
+
   return (
-    <Fragment>
+    <CartProvider>
       <Container>
-        <Header />
-        {/* <Products /> */}
-        <Cart />
+        <Header onShowCart={showCartHandler} onHideCart={hideCartHandler} />
+        {!showCart && <Products />}
+        {showCart && <Cart />}
       </Container>
       {/* <Footer /> */}
-    </Fragment>
+    </CartProvider>
   );
 }
 
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,23 +1,43 @@
+import { useContext } from "react";
+
 import testLogo from "../../assets/testlogo.png";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import classes from "./Header.module.css";
 import { BiCartAlt } from "react-icons/bi";
+import Badge from "react-bootstrap/Badge";
+import CartContext from "../../store/cart-context";
 
-const Header = () => {
+const Header = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numCartItems = cartCtx.items.reduce((num, item) => {
+    return num + item.amount;
+  }, 0);
+
+  const showBadge =
+    numCartItems !== 0 ? (
+      <Badge bg="secondary" className={classes.badge}>
+        {numCartItems}
+      </Badge>
+    ) : (
+      ""
+    );
+
   return (
     <Navbar collapseOnSelect expand="lg" id={classes.navbar}>
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand onClick={props.onHideCart} href="#">
           <img src={testLogo} className={classes.logo} alt="Company logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto"></Nav>
           <Nav>
-            <Nav.Link eventKey={2} href="#memes" id={classes.cart}>
+            <Nav.Link eventKey={2} onClick={props.onShowCart} id={classes.cart}>
               Cart
+              {showBadge}
               <BiCartAlt id={classes.icon} />
             </Nav.Link>
           </Nav>
